f38ad2ffa6993f2c922a8c0fda86b23c6e4bc405
Mauricio Gonzalez <mauriciogonzalezdeveloper@gmail.com>
2026-09-21 20:58:44 -0500

    feat add custom error

---
+++ src/domain/errors/custom.error.ts
@@ -0,0 +1,31 @@
+
+
+export class CustomError extends Error {
+
+    constructor(
+        public readonly statusCode: number,
+        public readonly message: string
+    ) {
+        super(message)
+    }
+
+    static badRequest(message: string) {
+        return new CustomError(400, message)
+    }
+
+    static unathorized(message: string) {
+        return new CustomError(401, message)
+    }
+
+    static forbiden(message: string) {
+        return new CustomError(403, message)
+    }
+
+    static notFound(message: string) {
+        return new CustomError(404, message)
+    }
+
+    static internalSever(message: string) {
+        return new CustomError(500, message)
+    }
+}
